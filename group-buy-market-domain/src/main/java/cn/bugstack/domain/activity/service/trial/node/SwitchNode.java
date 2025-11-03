61c43ff061497a9227912714be032673a268076b
yj <2020946884@qq.com>
2025-11-03 23:00:21 +0800

    init

---
+++ group-buy-market-domain/src/main/java/cn/bugstack/domain/activity/service/trial/node/SwitchNode.java
@@ -0,0 +1,35 @@
+package cn.bugstack.domain.activity.service.trial.node;
+
+import cn.bugstack.domain.activity.model.entity.MarketProductEntity;
+import cn.bugstack.domain.activity.model.entity.TrialBalanceEntity;
+import cn.bugstack.domain.activity.service.trial.AbstractGroupBuyMarketSupport;
+import cn.bugstack.domain.activity.service.trial.factory.DefaultActivityStrategyFactory;
+import cn.bugstack.types.design.framework.tree.StrategyHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 开关节点
+ * @create 2024-12-14 14:27
+ */
+@Slf4j
+@Service
+public class SwitchNode extends AbstractGroupBuyMarketSupport<MarketProductEntity, DefaultActivityStrategyFactory.DynamicContext, TrialBalanceEntity> {
+
+    @Resource
+    private MarketNode marketNode;
+
+    @Override
+    public TrialBalanceEntity doApply(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        return router(requestParameter, dynamicContext);
+    }
+
+    @Override
+    public StrategyHandler<MarketProductEntity, DefaultActivityStrategyFactory.DynamicContext, TrialBalanceEntity> get(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        return marketNode;
+    }
+
+}
